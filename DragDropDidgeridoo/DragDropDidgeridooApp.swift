6a1d861cbb421406f003b9a060011d3e1db084ff
nutterfi <nutterfi@users.noreply.github.com>
2026-06-02 21:49:01 -0600

    Initial Commit

---
+++ DragDropDidgeridoo/DragDropDidgeridooApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragDropDidgeridooApp.swift
+//  DragDropDidgeridoo
+//
+//  Created by nutterfi on 6/2/26.
+//
+
+import SwiftUI
+
+@main
+struct DragDropDidgeridooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DragDropDidgeridooTests/DragDropDidgeridooTests.swift
@@ -0,0 +1,19 @@
+//
+//  DragDropDidgeridooTests.swift
+//  DragDropDidgeridooTests
+//
+//  Created by nutterfi on 6/2/26.
+//
+
+import Testing
+@testable import DragDropDidgeridoo
+
+struct DragDropDidgeridooTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
